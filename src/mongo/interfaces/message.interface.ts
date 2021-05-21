6c4e43d95c2575fadc881d230be61d74d7664379
Caio Racy <caioracy94@gmail.com>
2021-05-20 21:43:34 -0300

    Editing interface creation

---
+++ src/mongo/interfaces/message.interface.ts
@@ -0,0 +1,10 @@
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+
+export interface Message extends Document {
+    readonly _id: mongoose.Schema.Types.ObjectId;
+    readonly from: Number,
+    readonly target_id: Number,
+    readonly media: Boolean,
+    readonly message: String
+}
